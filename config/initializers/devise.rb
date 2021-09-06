6e50c301741eb9804dc101911a2efa96a61b0a8c
ivanz <xvanx84@gmail.com>
2021-09-07 01:21:07 +0300

    fix github oauth

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -271,8 +271,7 @@ Devise.setup do |config|
   # ==> OmniAuth
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
-  config.omniauth :github, ENV['GITHUB_ID'], ENV['GITHUB_SECRET'], scope: 'user:email'
-  # config.omniauth :github, 'bea9c60e790affad8804', '331c3987bcc72d4c4aaecc09c864ffbca4f3da50', scope: 'user,public_repo'
+  config.omniauth :github, ENV['GITHUB_ID'], ENV['GITHUB_SECRET'], scope: 'user:email', provider_ignores_state: Rails.env.development?
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
