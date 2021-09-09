ee13660a2b628fa707022d74d222a7498c82cd9f
ivanz <xvanx84@gmail.com>
2021-09-09 14:10:18 +0300

    rm config hosts

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -18,13 +18,9 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def auth_handling
     auth_data = request.env['omniauth.auth']
     return redirect_to new_user_registration_url if auth_data.empty?
-    puts auth_data
+
     @user = User.from_omniauth(auth_data)
-    puts @user
-    puts '------------------------------------------'
-    puts "devise.#{auth_data['provider']}_data"
-    puts '---------------------------------------------'
-    puts auth_data['provider'].capitalize
+
     if @user.persisted?
       sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: auth_data['provider'].capitalize) if is_navigational_format?
--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,5 @@ module TodoApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    # config.hosts << "ae43-185-119-0-128.ngrok.io"
   end
 end
